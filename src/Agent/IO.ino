64f9abdc5e8d195a1aa9d1bb61dbeabb141b7fce
john smith <john@john.john>
2019-09-05 19:08:11 +1000

    POC for colour detection proves we don't need these pins, delete

--- src/Agent/IO.ino
+++ src/Agent/IO.ino
@@ -3,17 +3,14 @@
 
 void ConfigureSensor(SensorModel *sensor)
 {
-    pinMode(sensor->s0Pin, OUTPUT);
-    pinMode(sensor->s1Pin, OUTPUT);
     pinMode(sensor->s2Pin, OUTPUT);
     pinMode(sensor->s3Pin, OUTPUT);
     pinMode(sensor->outputPin, INPUT);
-    digitalWrite(sensor->s0Pin, HIGH);
-    digitalWrite(sensor->s1Pin, LOW);
 }
 
 int GetFrequency(SensorModel *sensor, bool s2_signal, bool s3_signal)
 {
+    Serial.println(sensor->outputPin);
     digitalWrite(sensor->s2Pin, s2_signal);
     digitalWrite(sensor->s3Pin, s3_signal);
     return pulseIn(sensor->outputPin, LOW);
